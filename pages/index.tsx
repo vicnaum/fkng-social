46267c94782d6d3d26d7cc5557186ef0c89e6cd0
vicnaum <vicnaum@gmail.com>
2025-04-28 09:19:54 +0200

    Add random slogans

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import styles from "../styles/Home.module.css";
 import { FaGithub, FaTwitter } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
 import FKNGHead from "../components/Head";
+import RandomSlogan from "../components/RandomSlogan";
 
 const Home: NextPage = () => {
   return (
@@ -13,7 +14,7 @@ const Home: NextPage = () => {
       <div className={styles.main}>
         <div className={styles.content}>
           <h1 className={styles.logo}>FKNG.SOCIAL</h1>
-          <p className={styles.slogan}>MAKING SOCIAL FKNG GREAT AGAIN</p>
+          <RandomSlogan />
         </div>
         <div className={styles.social}>
           <a href="mailto:contact@fkng.pro" rel="noreferrer" title="email">
